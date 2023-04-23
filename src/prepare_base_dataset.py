29ace39bc575434c1789b506f8a322fbba5e96a4
tuliochiodi <tuliochiodi@gmail.com>
2023-04-23 15:37:28 -0300

    prepare_base_dataset modified to slice silence from beginning of RIR 64

--- src/prepare_base_dataset.py
+++ src/prepare_base_dataset.py
@@ -2,6 +2,7 @@ import subprocess
 import shutil
 import zipfile
 from pathlib import Path
+import soundfile as sf
 
 command = "zenodo_get 10.5281/zenodo.7818761"
 subprocess.call(command, shell=True)
@@ -37,3 +38,13 @@ for filename in filenames:
 # remove .ini files
 for filename in dst_dir.glob("**/*.ini"):
     filename.unlink()
+
+# remove silence from the beginning of RIR 64
+rir_64 = dst_dir / "rir" / "rir_0064.wav"
+
+start_time = 0.82
+start_samples = int(start_time * 48000)
+
+y, sr = sf.read(rir_64, start=start_samples)
+sf.write(rir_64, y, sr)
+
